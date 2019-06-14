4f9adea941baf95a69d62f4a943df37e254b5934
root <root@ip-172-31-40-41.ec2.internal>
2019-06-14 00:03:48 +0000

    updating directories

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$('#logo-button').on('click', function() {
+    window.location.assign("https://www.paulolupo.live");
+});
+
+$('#aboutMe').on('click', function() {
+    window.location.assign("https://www.paulolupo.live");
+});
+$('#contactMe').on('click', function() {
+    window.location.assign("https://www.paulolupo.live");
+});
+$('#links').on('click', function() {
+    window.location.assign("https://www.paulolupo.live");
+});
+$('#projects').on('click', function() {
+    window.location.assign("https://www.paulolupo.live");
+});
